2ecf40efcd946cf00913bb3ecc07cf8a0f6ba7b4
Gavin Chen <gavinchen@ccs.k12.in.us>
2022-12-01 11:53:10 -0500

    day 1

---
+++ 2022/Day 1/Part 2.py
@@ -0,0 +1,12 @@
+calories = [int(calorie) if calorie != "\n" else None for calorie in open("input.txt").readlines()]
+amounts = []
+
+cur = 0
+for cal in calories:
+    if cal != None:
+        cur += cal
+    else:
+        amounts.append(cur)
+        cur = 0
+
+print(sum(sorted(amounts)[-3:]))
