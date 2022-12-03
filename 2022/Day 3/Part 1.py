ce2ccb107853793d0772c59c2c70cb10e49e7495
tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-12-03 13:17:44 -0500

    part 1

---
+++ 2022/Day 3/Part 1.py
@@ -0,0 +1,19 @@
+from string import ascii_letters
+
+input = [[x[:len(x) // 2], x[len(x) // 2:].rstrip()] for x in open("input.txt").readlines()]
+
+common = []
+
+for sack in input:
+    for letter in sack[0]:
+        if letter in sack[1]:
+            print(letter)
+            common.append(letter)
+            break
+
+priorities = {}
+
+for i, letter in enumerate(ascii_letters):
+    priorities.update({letter: i + 1})
+
+print(sum([priorities[letter] for letter in common]))
