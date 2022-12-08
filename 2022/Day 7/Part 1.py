bb559679452209d1f289aa39522c33070ea28890
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-12-08 14:50:36 -0500

    finally

--- 2022/Day 7/Part 1.py
+++ 2022/Day 7/Part 1.py
@@ -1,31 +1,6 @@
-from hashlib import new
 
+input = [node.rstrip() for node in open("input.txt").readlines()]
 
-input = [
-"$ cd /", #open("input.txt").readlines()
-"$ ls",
-"dir a",
-"14848514 b.txt",
-"8504156 c.dat",
-"dir d",
-"$ cd a",
-"$ ls",
-"dir e",
-"29116 f",
-"2557 g",
-"62596 h.lst",
-"$ cd e",
-"$ ls",
-"584 i",
-"$ cd ..",
-"$ cd ..",
-"$ cd d",
-"$ ls",
-"4060174 j",
-"8033020 d.log",
-"5626152 d.ext",
-"7214296 k"
-]
 input = [line.split(" ") for line in input]
 
 class Node:
@@ -63,28 +38,30 @@ directories: list[Directory] = []
 
 for i in range(1, len(input)):
     line = input[i]
-    if line[1] == "cd":
-        print(line)
+    if line[1] == "ls":
+        continue
+    elif line[1] == "cd":
         if line[2] == "..":
             current_directory = current_directory.parent
         else:
-            print([c.name for c in current_directory.children])
             for child in current_directory.children:
-                if isinstance(child, Directory) and child.name == list[2]:
+                if isinstance(child, Directory) and child.name == line[2]:
                     current_directory = child
     else:
         if line[0] == "dir":
             new_directory = Directory(line[1], current_directory)
             current_directory.add(new_directory)
             directories.append(new_directory)
-            print(new_directory.name + " " + new_directory.parent.name + " " + str([c.name for c in current_directory.children]))
         elif line[0] != "$":
             current_directory.add(File(line[1], int(line[0]), current_directory))
+            
+#print([{c.name: c.size()} for c in directories])
 
 sum = 0
 
 for directory in directories:
-    if s := directory.size() <= 100000:
+    print({directory.name: directory.size()})
+    if (s := directory.size()) <= 100000:
         sum += s
 
 print(sum)
