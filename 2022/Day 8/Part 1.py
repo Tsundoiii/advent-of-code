56f80cd863d5352ff98b7e1c6312d3786cbbe3e2
tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-12-09 19:44:13 -0500

    finally

--- 2022/Day 8/Part 1.py
+++ 2022/Day 8/Part 1.py
@@ -1,14 +1,43 @@
 input = [[int(tree) for tree in treerow.rstrip()] for treerow in open("input.txt").readlines()]
+columns = []
+
+for j in range(len(input)):
+    row = []
+    for treerow in input:
+        row.append(treerow[j])
+    columns.append(row)
 
 visible = 0
 
-def visible_in_row(forest):
-    pass
+def before(row, index):
+    if index == 0:
+        return []
+    elif index == 1:
+        return [row[0]]
+    else:
+        return row[:index]
 
-def visibie_in_column(forest):
-    pass
+def after(row, index):
+    if index == len(row) - 1:
+        return []
+    else:
+        return row[index + 1:]
 
-for treerow in input:
-    for tree in treerow:
-        if input.index(treerow) == 0 or input.index(treerow) == len(treerow) - 1:
+idk = []
+
+for i, treerow in enumerate(input):
+    d = []
+    for j, tree in enumerate(treerow):
+        try:
+            if max(before(treerow, j)) < tree or max(after(treerow, j)) < tree or max(before(columns[j], i)) < tree or max(after(columns[j], i)) < tree:
+                #treerow[j] = True
+                d.append(True)
+                visible += 1
+            else:
+                d.append(tree)
+        except ValueError:
             visible += 1
+            d.append(True)
+    idk.append(d)
+
+print(visible)
